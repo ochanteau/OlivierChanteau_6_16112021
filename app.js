6cac9b586594fc2a43fe3404c8af6839f96eb4c0
CHANTEAU <chanteau.o@gmail.com>
2021-11-17 18:21:39 +0100

    mongoose, connection base de donnée , et variable d environnement

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
+// import express et mise en place
 const express = require('express');
-
 const app = express();
 
+// import module Dotenv pour les varibales d'environnement
+const dotenv = require("dotenv");
+dotenv.config();
+
+// import du pacquet mongoose et connection à la base de donnée
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.jiuzr.mongodb.net/cluster0?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// middleware pour autoriser les connections à l'API
+// (vérifier si je laisse PATCH ET OPTIONS)
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+// app.use(express.json());
+
+// app.post("/",(req, res, next) => {
+//   console.log(req.body);
+//   res.status(201).json({
+//     message: 'Objet créé !'
+//   });
+// });
+
+
+
 
 app.use((req, res, next) => {
   res.json({ message: 'Votre requête a bien été reçue !' });
