b69b51261d780931e3c7da2a0331165764e7a461
CHANTEAU <chanteau.o@gmail.com>
2021-12-06 07:38:17 +0100

    maj commentaires models

--- models/sauce.js
+++ models/sauce.js
@@ -1,6 +1,10 @@
+// import module mongoose
 const mongoose = require('mongoose');
+// import plugin de remontée des erreurs de la base de donnée 
 const MongooseErrors = require('mongoose-errors')
 
+
+// creation du schéma Sauce
 const sauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
   name: { type: String, required: true },
@@ -15,7 +19,7 @@ const sauceSchema = mongoose.Schema({
   usersDisliked: { type: [String],required: true }
 });
 
-
+//  configuration du plugin mongoose-errors
 sauceSchema.plugin(MongooseErrors);
 
 
--- models/user.js
+++ models/user.js
@@ -1,12 +1,18 @@
+// import module mongoose
 const mongoose = require('mongoose');
+// import plugin complementaire mongoose pour l unicité des mails
 const uniqueValidator = require('mongoose-unique-validator');
+// import plugin de remontée des erreurs de la base de donnée 
 const MongooseErrors = require('mongoose-errors')
 
+
+// creation du schéma utilisateur
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
+//  configuration des plugins complémentaires a mongooose
 userSchema.plugin(uniqueValidator);
 userSchema.plugin(MongooseErrors);
 
